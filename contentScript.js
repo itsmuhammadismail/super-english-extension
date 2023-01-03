5a3cf25c644fe8b47c00bbf077393eddf25f42dd
Muhammad Ismail <itsmuhammadismail@gmail.com>
2023-01-03 17:36:04 +0500

    added in reply

--- contentScript.js
+++ contentScript.js
@@ -70,66 +70,71 @@
   };
 
   const addGmailButton = async () => {
-    const input = document.querySelector(".Am");
-    input.parentElement.style.position = "relative";
+    const inputs = document.querySelectorAll(".Am");
+    for (let i = 0; i < inputs.length; i++) {
+      let input = inputs[i];
+      input.parentElement.style.position = "relative";
+      const superList = input.parentElement.querySelectorAll(".superlist");
 
-    const superList = input.parentElement.querySelectorAll(".superlist");
-    if ([...superList].length > 0) {
-      for (let i = 0; i < [...superList].length; i++) {
-        input.parentElement.removeChild(superList[i]);
+      if ([...superList].length > 0) {
+        for (let i = 0; i < [...superList].length; i++) {
+          input.parentElement.removeChild(superList[i]);
+        }
       }
+      input.parentNode.style.position = "relative";
+
+      const createElement = document.createElement("div");
+      createElement.classList.add("superlist");
+
+      createElement.innerHTML = `
+        <div class="list">
+          <img id="super_eng_btn" class="other_img" src="${chrome.runtime.getURL(
+            "assets/eng.svg"
+          )}"
+          />
+          <img id="super_urdu_btn" class="other_img" src="${chrome.runtime.getURL(
+            "assets/urdu.svg"
+          )}" />
+          <img id="super_formal_btn" class="other_img" src="${chrome.runtime.getURL(
+            "assets/formal.svg"
+          )}" />
+          <img id="super_casual_btn" class="other_img" src="${chrome.runtime.getURL(
+            "assets/casual.svg"
+          )}" />
+          <img class="super_img" src="${chrome.runtime.getURL(
+            "assets/active.svg"
+          )}" />
+        </div>
+      `;
+
+      input.parentElement.appendChild(createElement);
+
+      document
+        .querySelector("#super_eng_btn")
+        .addEventListener("click", () =>
+          translate({ input: input, language: "english", isTextField: false })
+        );
+
+      document.querySelector("#super_urdu_btn").addEventListener("click", () =>
+        translate({
+          input: input,
+          language: "roman urdu",
+          isTextField: false,
+        })
+      );
+
+      document
+        .querySelector("#super_formal_btn")
+        .addEventListener("click", () =>
+          changeTone({ input: input, tone: "professional", isTextField: false })
+        );
+
+      document
+        .querySelector("#super_casual_btn")
+        .addEventListener("click", () =>
+          changeTone({ input: input, tone: "casual", isTextField: false })
+        );
     }
-    input.parentNode.style.position = "relative";
-
-    const createElement = document.createElement("div");
-    createElement.classList.add("superlist");
-
-    createElement.innerHTML = `
-      <div class="list">
-        <img id="super_eng_btn" class="other_img" src="${chrome.runtime.getURL(
-          "assets/eng.svg"
-        )}"
-        />
-        <img id="super_urdu_btn" class="other_img" src="${chrome.runtime.getURL(
-          "assets/urdu.svg"
-        )}" />
-        <img id="super_formal_btn" class="other_img" src="${chrome.runtime.getURL(
-          "assets/formal.svg"
-        )}" />
-        <img id="super_casual_btn" class="other_img" src="${chrome.runtime.getURL(
-          "assets/casual.svg"
-        )}" />
-        <img class="super_img" src="${chrome.runtime.getURL(
-          "assets/active.svg"
-        )}" />
-      </div>
-    `;
-
-    input.parentElement.appendChild(createElement);
-
-    document
-      .querySelector("#super_eng_btn")
-      .addEventListener("click", () =>
-        translate({ input: input, language: "english", isTextField: false })
-      );
-
-    document
-      .querySelector("#super_urdu_btn")
-      .addEventListener("click", () =>
-        translate({ input: input, language: "roman urdu", isTextField: false })
-      );
-
-    document
-      .querySelector("#super_formal_btn")
-      .addEventListener("click", () =>
-        changeTone({ input: input, tone: "professional", isTextField: false })
-      );
-
-    document
-      .querySelector("#super_casual_btn")
-      .addEventListener("click", () =>
-        changeTone({ input: input, tone: "casual", isTextField: false })
-      );
   };
 
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
@@ -140,6 +145,22 @@
       try {
         document.querySelector(".Am");
         addGmailButton();
+
+        document
+          .querySelector(".T-I")
+          .addEventListener("click", () => addGmailButton());
+        document.querySelector(".zA").addEventListener("click", () => {
+          setTimeout(() => {
+            document
+              .querySelector(".ams")
+              .addEventListener("click", () =>
+                setTimeout(() => addGmailButton(), 1000)
+              );
+          }, 1000);
+        });
+        // document
+        //   .querySelector(".ams")
+        //   .addEventListener("click", () => addGmailButton());
       } catch (e) {
         console.log("Not chrome");
       }
